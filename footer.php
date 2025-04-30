c1d0582dbe869f2ecb70cf1e74541d350d72aad1
AhmadNidzom <thecrimsen99@gmail.com>
2025-04-30 12:04:35 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,39 @@
+<footer>
+        <section class="footer-logo">
+            <img src="../asset/image/logap.png" alt="logo" width="180" height="180" style="display: block;">
+            <span class="copy">&copy;DVNStore 2024</span>
+            <section class="i">
+                <a class="fab fa-instagram"></a>
+                <a class="fab fa-facebook-f"></a>
+                <a class="fab fa-linkedin-in"></a>
+                <a class="fab fa-x-twitter"></a>
+            </section>
+        </section>
+        <ul class="footer">
+            <p>Dashboard</p>
+            <li><a href="Dashboard.php">Home</a></li>
+            <li><a href="Dashboard Game.php">Game</a></li>
+            <li><a href="Dashboard Apps.php">Apps</a></li>
+            <li><a href="dashboardfilm.php">Film</a></li>
+        </ul>
+        <ul class="footer">
+            <p>Top Kategori</p>
+            <li><a href="Top Game.php">Top Game</a></li>
+            <li><a href="Top Apps.php">Top Apps</a></li>
+            <li><a href="Top Film.php">Top Film</a></li>
+        </ul>
+    </footer>
+    <section class="fotbwh">
+        <p>About Us</p>
+        <p>Security Policy</p>
+        <P>Bahasa Indonesia</P>
+    </section>
+    <section class="kondusif">
+        <span class="copy-k">&copy;DVNStore 2024</span>
+        <section class="o">
+            <a class="fab fa-instagram"></a>
+            <a class="fab fa-facebook-f"></a>
+            <a class="fab fa-linkedin-in"></a>
+            <a class="fab fa-x-twitter"></a>
+        </section>
+    </section>
---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+        <nav>
+            <div class="logo">
+                <img src="../asset/image/logap.png" alt="logo" width="35" height="40">
+                <h1>DVNStore</h1>
+            </div>
+            <input type="text" placeholder="Telusuri.......">
+            <div class="ic">
+                <a href ="unduhan.php" class="material-symbols-outlined icon">download</a>
+                <a href= "logout.php"class="material-symbols-outlined icon">logout</a>
+            </div>
+        </nav>
+    </header>
